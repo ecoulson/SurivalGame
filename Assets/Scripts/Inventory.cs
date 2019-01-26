af58cfdbd4888e3d0c8af253c404f6c47b486d19
Evan Coulson <evan.m.coulson@gmail.com>
2019-01-25 19:00:52 -0800

    first commit

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject InventoryGUI;
+
+    private bool isVisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isVisible = false;
+        InventoryGUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldDisplayInventory()) {
+            isVisible = !isVisible;
+            InventoryGUI.SetActive(isVisible);
+        }
+    }
+
+    private bool ShouldDisplayInventory()
+    {
+        return Input.GetKeyUp(KeyCode.E) || 
+                (isVisible && Input.GetKeyUp(KeyCode.Escape));
+    }
+}
